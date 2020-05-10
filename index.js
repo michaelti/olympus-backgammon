a36d406fdb544f0757d60cb5225dbd075ef5e5ef
Michael <m@michaelti.ca>
2020-05-10 16:04:29 -0400

    Add Socket.IO boilerplate / demos (#1)
    * Add socket.io backend code for SocketDemo frontend component

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
-const io = require('socket.io')();
-
-io.on('connection', (client) => {
-    console.log('Client connected.');
+const io = require('socket.io')(process.env.PORT, {
+    serveClient: false,
+    pingInterval: 2000
 });
 
-io.listen(process.env.PORT);
+io.on('connection', (clientSocket) => {
+    console.log(`Client connected with id ${clientSocket.id}`);
+
+    clientSocket.on('disconnect', (reason) => {
+        console.log(`Client disconnected with id ${clientSocket.id}, reason: ${reason}`);
+    });
+
+    clientSocket.emit('message', 'Welcome to the default namespace!');
+});
+
+// Game namespace
+const nspGame = io.of('/game');
+nspGame.on('connection', (clientSocket) => {
+    clientSocket.emit('message', 'Welcome to the /game namespace!');
+});
+
+// Chat namespace
+const nspChat = io.of('/chat');
+nspChat.on('connection', (clientSocket) => {
+    clientSocket.emit('message', 'Welcome to the /chat namespace!');
+
+    clientSocket.on('join-room', (roomName) => {
+        clientSocket.join(roomName, () => {
+            nspChat.to(roomName).emit('message', `
+                ${clientSocket.id} has joined ${roomName}
+            `);
+        });
+    });
+
+    clientSocket.on('leave-room', (roomName) => {
+        clientSocket.leave(roomName, () => {
+            nspChat.to(roomName).emit('message', `
+                ${clientSocket.id} has left ${roomName}
+            `);
+        });
+    });
+
+    clientSocket.on('disconnecting', () => {
+        Object.keys(clientSocket.rooms).forEach((roomName) => {
+            nspChat.to(roomName).emit('message', `${clientSocket.id} has disconnected.`)
+        });
+    });
+});
