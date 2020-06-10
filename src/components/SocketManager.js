0e2fc5ef384e964fbfa5c065ffb2e2ce51cc917d
Michael <m@michaelti.ca>
2020-06-09 20:54:54 -0400

    Refactor/document websocket code in SocketManager (#10)
    - Compatible with [#6 on back-end](https://github.com/michaelti/olympus-backgammon-backend/pull/6)

--- src/components/SocketManager.js
+++ src/components/SocketManager.js
@@ -10,52 +10,47 @@ function SocketManager() {
     const [boardState, setBoardState] = useState(null);
 
     useEffect(() => {
-        // Receive successful joined room
-        socket.on('event/joined-room', (roomName) => {
-            setRoomName(roomName);
-        });
+        /* GAME EVENT LISTENERS */
+        
+        // Game event: Receive updated board object
+        socket.on('game/update-board', (board) => setBoardState(board));
 
-        // Receive failed join room
-        socket.on('event/failed-join-room', (roomName) => {
-            console.log(`Failed to join room "${roomName}" because it does not exist.`);
-        });
-
-        // Receive updated board object
-        socket.on('game/update-board', (board) => {
-            setBoardState(board);
-        });
     }, [socket]);
 
-    // Start a room
+    /* ROOM EVENT EMITTERS */
+
+    // Start a room, receive confirmation and room name
     const startRoom = () => {
-        socket.emit('event/start-room');
+        socket.emit('event/start-room', (acknowledgement) => {
+            if (!acknowledgement.ok) {
+                console.log(`Failed to start room "${acknowledgement.roomName}".`)
+            } else {
+                setRoomName(acknowledgement.roomName);
+            }
+        });
     };
 
-    // Join a room
+    // Join a room, receive confirmation and room name
     const joinRoom = (roomName) => {
-        socket.emit('event/join-room', roomName);
-    };
-
-    // Send a submove to the server, get a new board state
-    const doSubmove = (from, to) => {
-        socket.emit('game/submove', from, to, (board) => {
-            setBoardState(board);
+        socket.emit('event/join-room', roomName, (acknowledgement) => {
+            if (!acknowledgement.ok) {
+                console.log(`Failed to join room "${acknowledgement.roomName}".`);
+            } else {
+                setRoomName(acknowledgement.roomName);
+            }
         });
     };
 
-    // Tell the server our turn is complete, get a new board state
-    const applyTurn = () => {
-        socket.emit('game/apply-turn', (board) => {
-            setBoardState(board);
-        });
-    };
+    /* GAME EVENT EMITTERS */
 
-    // Tell the server to undo our submoves, get a new board state
-    const undoTurn = () => {
-        socket.emit('game/undo', (board) => {
-            setBoardState(board);
-        });
-    };
+    // Send a submove to the server
+    const doSubmove = (from, to) => socket.emit('game/submove', from, to);
+
+    // Tell the server our turn is complete
+    const applyTurn = () => socket.emit('game/apply-turn');
+
+    // Tell the server to undo our submoves
+    const undoTurn = () => socket.emit('game/undo');
 
     return (
         <div>
