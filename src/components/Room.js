c96b3fe9e6a3c829383933f49a01181d6623fe1a
Michael <m@michaelti.ca>
2020-06-26 16:20:51 -0400

    Display the player colour received from the backend when joining room (#22)
    Adds a badge to display the player colour information provided from backend#16

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,11 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { socketEmit } from "../api";
 import { Container } from "reactstrap";
+import { Player } from "../util";
+import { Badge } from "reactstrap";
 import Game from "./Game";
 
 function Room({ setRoomName }) {
     const { roomName } = useParams();
+    const [player, setPlayer] = useState(undefined);
 
     useEffect(() => {
         socketEmit("event/join-room", roomName, (acknowledgement) => {
@@ -13,12 +16,16 @@ function Room({ setRoomName }) {
                 console.log(`Failed to join room "${acknowledgement.roomName}".`);
             } else {
                 setRoomName(acknowledgement.roomName);
+                setPlayer(acknowledgement.player);
             }
         });
     }, [roomName, setRoomName]);
 
     return (
         <Container className="py-5">
+            <Badge className="mb-3">
+                {player ? `Playing as ${Player.properties[player].colorName}` : "Spectating"}
+            </Badge>
             <Game />
         </Container>
     );
