37c2a2a9537d4b3717f6380cc83fd70b32dc88ad
Michael <m@michaelti.ca>
2021-01-10 20:37:03 -0500

    Remove unused exports

--- src/index.js
+++ src/index.js
@@ -1,28 +1,10 @@
-const {
-    Board,
-    Pip,
-    Move,
-    Player,
-    Variant,
-    reverseMove,
-    rollDie,
-    clamp,
-    pipDistance,
-} = require("./game");
+const { Variant, Player, Board, reverseMove, rollDie } = require("./game");
 
-const { range } = require("./util");
-
-exports.Board = Board;
-exports.Pip = Pip;
-exports.Move = Move;
-exports.Player = Player;
 exports.Variant = Variant;
+exports.Player = Player;
+exports.Board = Board;
 exports.reverseMove = reverseMove;
 exports.rollDie = rollDie;
-exports.clamp = clamp;
-exports.pipDistance = pipDistance;
-
-exports.range = range;
 
 exports.portes = require("./variants/portes");
 exports.plakoto = require("./variants/plakoto");
