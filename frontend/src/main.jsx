88e015c7d96a1e04cdf7e954d41e6c912446ef3c
Michael <mdismatsek@brainstation.io>
2023-02-19 22:06:56 +0000

    Remove unused console log

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 
-console.log(import.meta.env.VITE_BACKEND_URL);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <App />
