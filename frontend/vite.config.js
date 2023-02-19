292c11f0548ac59adc1e7b7cfdf67e0a9f968e78
Michael <mdismatsek@brainstation.io>
2023-02-19 23:39:09 +0000

    Add legacy plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
         open: "/",
     },
-    plugins: [react()],
+    plugins: [react(), legacy()],
     optimizeDeps: {
         include: ["olympus-bg"],
-        global: "globalThis",
     },
     build: {
         commonjsOptions: {
