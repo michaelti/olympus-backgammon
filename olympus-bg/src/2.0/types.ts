a35ec38a2dc169df75dc159551a15489f4270f37
Michael <m@michaelti.ca>
2024-12-16 19:27:29 +0000

    Add 'data' types for transport (to discuss)

--- olympus-bg/src/2.0/types.ts
+++ olympus-bg/src/2.0/types.ts
@@ -5,3 +5,44 @@ export enum Player {
 }
 
 export type PlayerBW = Player.black | Player.white;
+
+// TODO: consider whethere this is the right approach
+// (for snapshots/restoration of game state)
+export type GameData = {
+    player: PlayerBW;
+    moves: MoveData[];
+    dice: DiceData;
+    pips: PipData[];
+    bar: BarData;
+    off: OffData;
+};
+
+export type MoveData = {
+    from: number;
+    to: number;
+    dieUsed: number;
+};
+
+export type DiceData = {
+    initial: [number, number] | [number, number, number, number];
+    remaining: number[];
+    isDoubles: boolean;
+};
+
+type PipData = {
+    size: number;
+    owner: Player;
+    isPinned: boolean;
+};
+
+type BarData = {
+    [Player.black]: number;
+    [Player.white]: number;
+};
+
+type OffData = {
+    [Player.black]: number;
+    [Player.white]: number;
+};
+// END TODO
+//
