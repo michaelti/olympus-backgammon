2c2a45dbec1bc8b8c02dad5c6a28290288c25f08
Michael <m@michaelti.ca>
2024-12-21 17:03:08 +0000

    Refactor utils

--- olympus-bg/src/2.0/util.ts
+++ olympus-bg/src/2.0/util.ts
@@ -19,15 +19,25 @@ export function otherPlayer(player: PlayerBW): PlayerBW {
 
 /**
  * Returns value clamped to the inclusive range of 0–25
- * Ex: (-3) => 0; (15) => 15; (29) => 25
  */
-export const clamp = (to: number) => (to < 0 ? 0 : to > 25 ? 25 : to);
+export function clamp(to: number): number {
+    if (to < 0) to = 0;
+    if (to > 25) to = 25;
+    return to;
+}
 
-// Returns the distance between two pips (1–12)
-export const pipDistance = function (from: number, to: number) {
-    const dist = Math.abs(to - from);
-    return dist <= 12 ? dist : 24 - dist;
-};
+/**
+ * Returns the distance between two pips (1-12)
+ */
+export function pipDistance(from: number, to: number): number {
+    const distance = Math.abs(to - from);
+    return distance <= 12 ? distance : 24 - distance;
+}
 
-export const range = (start: number, end: number, length = end - start + 1) =>
-    Array.from({ length }, (_, i) => start + i);
+/**
+ * Returns an array of integers within the inclusive range of start to end
+ */
+export function range(start: number, end: number): number[] {
+    const length = end - start + 1;
+    return Array.from({ length }, (_, i) => start + i);
+}
