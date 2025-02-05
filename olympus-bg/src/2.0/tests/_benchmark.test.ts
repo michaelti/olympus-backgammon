d58765b83348f66dcd7be780a6f8c4f3185e3a69
Michael <m@michaelti.ca>
2025-02-05 20:32:59 +0000

    Clean up benchmark test

--- olympus-bg/src/2.0/tests/_benchmark.test.ts
+++ olympus-bg/src/2.0/tests/_benchmark.test.ts
@@ -1,40 +1,9 @@
 import { describe, test } from "vitest";
-import { Game } from "../Game.js";
+import { Portes } from "../Portes.js";
 import { Board } from "../../game.js";
 import { clone } from "ramda";
-import { GameData, PlayerBW } from "../types.js";
 
-class MockGame extends Game {
-    constructor(initial: GameData | { player: PlayerBW }) {
-        super(initial);
-
-        if ("pips" in initial) {
-            return;
-        }
-
-        // Black moves towards pip 1 (decreasing)
-        // White moves towards pip 24 (increasing)
-        this.pips[25].set(0, "black");
-        this.pips[24].set(2, "black");
-        this.pips[19].set(5, "white");
-        this.pips[17].set(3, "white");
-        this.pips[13].set(5, "black");
-        this.pips[12].set(5, "white");
-        this.pips[8].set(3, "black");
-        this.pips[6].set(5, "black");
-        this.pips[1].set(2, "white");
-        this.pips[0].set(0, "white");
-    }
-
-    isMoveValid(from: number, to: number): boolean {
-        return from < to;
-    }
-
-    doMove(from: number, to: number): void {
-        this.pips[from].size--;
-        this.pips[to].size++;
-    }
-}
+// TODO: Remove this or do it properly with vitest `bench`
 
 function stressTest(fn: () => void) {
     // Enough times to force vitest to show the 'slow test' time
@@ -45,27 +14,23 @@ function stressTest(fn: () => void) {
     }
 }
 
-describe(
-    "Approaches to cloning boards",
-    () => {
-        test("Old (Board) implementation", () => {
-            const board = Board();
-            const boards = [];
+describe("Approaches to cloning boards", { skip: true }, () => {
+    test("Old (Board) implementation", () => {
+        const board = Board();
+        const boards = [];
 
-            stressTest(() => {
-                const clonedBoard = clone(board);
-                boards.push(clonedBoard);
-            });
+        stressTest(() => {
+            const clonedBoard = clone(board);
+            boards.push(clonedBoard);
         });
+    });
 
-        test("New (Game) implementation", () => {
-            const game = new MockGame({ player: "black" });
-            const games = [];
-            stressTest(() => {
-                const clonedGame = new MockGame(game);
-                games.push(clonedGame);
-            });
+    test("New (Game) implementation", () => {
+        const game = new Portes({ player: "black" });
+        const games = [];
+        stressTest(() => {
+            const clonedGame = new Portes(game);
+            games.push(clonedGame);
         });
-    },
-    { skip: true },
-);
+    });
+});
