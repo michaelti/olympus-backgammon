bb1e6da9991eb4e3b7ae5d4a61f9fb22ee276ba2
Michael <m@michaelti.ca>
2025-02-05 20:41:42 +0000

    Remove tests from Game.test.ts

--- olympus-bg/src/2.0/tests/Game.test.ts
+++ olympus-bg/src/2.0/tests/Game.test.ts
@@ -1,27 +1,16 @@
 import { describe, expect, test } from "vitest";
-import { Game } from "../Game.js";
-import { GameData, TurnValidity } from "../types.js";
-import { Pip } from "../Pip.js";
+import { Portes } from "../Portes.js";
+import { GameData } from "../types.js";
 import { stringToPips } from "../util.js";
 
-// Mock game implementation because
-// abstract class can't be tested directly
-class MockGame extends Game {
-    isMoveValid(from: number, to: number): boolean {
-        return from < to;
-    }
-
-    doMove(from: number, to: number): void {
-        this.pips[from].size--;
-        this.pips[to].size++;
-    }
-}
+// Mock game implementation because abstract class can't be tested directly
+// TODO: have all game tests just use Portes/Plakoto/Fevga instead of faking it
+class MockGame extends Portes {}
 
 describe("constructor", () => {
     test("Initializes pips", () => {
         const game = new MockGame({ player: "black" });
         expect(game.pips).toHaveLength(26);
-        expect(game.pips[0]).toBeInstanceOf(Pip);
     });
 
     test("Makes a game with gameData", () => {
@@ -40,24 +29,5 @@ describe("constructor", () => {
         const game = new MockGame(gameData);
 
         expect(game).toMatchObject(gameData);
-    });
-});
-
-describe("getTurnValidity", () => {
-    test("Returns validZero if there are no possible moves", () => {
-        const game = new MockGame({
-            player: "black",
-            pips: stringToPips(`
-                0 0 0 0 0 0 2w 2w 2w 2w 2w 2w
-                0 0 0 0 0 0 0 0 0 0 0 0
-                1b
-            `),
-            bar: { white: 0, black: 0 },
-            off: { white: 0, black: 0 },
-            dice: { initial: [1, 2], remaining: [1, 2] },
-            moves: [],
-        });
-
-        expect(game.getTurnValidity()).toBe(TurnValidity.validZero);
     });
 });
