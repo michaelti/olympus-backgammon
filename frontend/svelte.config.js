b89ac14750bb2ce9f81c60090ed68833451c49d3
Michael <m@michaelti.ca>
2025-07-20 22:38:50 +0100

    Update frontend prettier config

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,12 +1,12 @@
-import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-node";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+    // Consult https://svelte.dev/docs/kit/integrations
+    // for more information about preprocessors
+    preprocess: vitePreprocess(),
+    kit: { adapter: adapter() },
 };
 
 export default config;
