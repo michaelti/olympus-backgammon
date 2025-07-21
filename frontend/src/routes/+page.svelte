159a935725195539f6c9365c5fe3a75d7a2692b6
Michael <m@michaelti.ca>
2025-07-21 01:18:36 +0100

    fixup! Proof of concept for Tavli in frontend

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,42 @@
-<h1 class="bg-stone">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import DicesIcon from "@lucide/svelte/icons/dices";
+    import { Tavli } from "olympus-bg";
+
+    const tavli = new Tavli();
+    const display = () => JSON.stringify(tavli, null, 4);
+
+    let string = $state(display());
+
+    const start = () => {
+        tavli.pickVariant("Portes");
+        string = display();
+    };
+
+    const startRollB = () => {
+        tavli.rollToStart("black");
+        string = display();
+    };
+</script>
+
+<div class="flex min-h-dvh flex-col items-center justify-center gap-8">
+    <h1 class="flex gap-1 text-lg font-bold">
+        <DicesIcon aria-hidden="true" />
+        Olympus Backgammon
+    </h1>
+
+    <div class="flex w-full justify-center overflow-y-auto rounded bg-stone-200 p-4 text-sm">
+        <pre>{string}</pre>
+    </div>
+
+    <div>
+        <button onclick={start} class="cursor-pointer rounded border border-stone-300 px-4 py-2">
+            Start game
+        </button>
+        <button
+            onclick={startRollB}
+            class="cursor-pointer rounded border border-stone-300 px-4 py-2"
+        >
+            Roll B
+        </button>
+    </div>
+</div>
