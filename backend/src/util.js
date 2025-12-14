850d89f562c5f96237becf914982041fc926997c
Michael <m@michaelti.ca>
2025-12-14 16:01:16 +0000

    Remove frontend-1.0 and backend

--- backend/src/util.js
+++
@@ -1,5 +0,0 @@
-// Return a random numeric string of length n
-export const randomNumeric = (length) => {
-    const num = Math.floor(Math.random() * 10 ** length);
-    return num.toString().padStart(length, "0");
-};
--- frontend-1.0/src/styles/globalStyles.js
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from "styled-components";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-export default createGlobalStyle`
-    /** Height convenience for app and its parents **/
-    html, body, #root, .App {
-        min-height: 100%;
-    }
-`;
