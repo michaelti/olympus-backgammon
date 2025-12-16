f574e449adec3c64626d1a7297e93abb250c955c
Michael <m@michaelti.ca>
2025-12-16 23:49:51 +0000

    Make rankBoard player-agnostic. Test getBestTurn.

--- olympus-bg/src/PortesBot.ts
+++ olympus-bg/src/PortesBot.ts
@@ -1,5 +1,6 @@
 import { Game } from "./Game.js";
 import { Move } from "./types.js";
+import { BAR, OFF, pipDistance, range } from "./util.js";
 
 /**
  * The "endgame" is when there is no possibility of either player being sent to the bar
@@ -23,9 +24,8 @@ function isEndGame(game: Game): boolean {
     return isEndGame;
 }
 
-// TODO: make this work for either player white or black
 /**
- * Heuristic function to rank a board state based on how "good" it is for Player.black
+ * Heuristic function to rank a board state based on how "good" it is for the current player
  * Returns a score (higher is better)
  */
 function rankBoard(game: Game): number {
@@ -33,22 +33,24 @@ function rankBoard(game: Game): number {
 
     if (isEndGame(game)) {
         // While in endgame, the priority should be moving all checkers to home quadrant
-        game.pips.forEach((pip, i) => {
+        for (const [i, pip] of game.pips.entries()) {
             // A penalty of 1 point per space from home is applied
-            const spacesFromHome = i < 6 ? 0 : i - 6;
+            const isHome = pipDistance(OFF[game.player], i) <= 6;
+            const distanceFromHome = pipDistance(OFF[game.player], i) - 6;
+            const penalty = isHome ? 0 : distanceFromHome;
 
             if (pip.owner === game.player) {
-                rank -= pip.size * spacesFromHome;
+                rank -= pip.size * penalty;
             }
-        });
+        }
     } else {
-        game.pips.forEach((pip, i) => {
+        for (const [i, pip] of game.pips.entries()) {
             // An open checker deducts points based on distance it could be sent back
             if (pip.owner === game.player && pip.size === 1) {
-                for (let j = i - 1; j >= 0; j--) {
-                    // If a white checker exists ahead
-                    if (game.pips[j].owner === "white") {
-                        rank -= 24 - i;
+                for (const j of range(i, OFF[game.player])) {
+                    // If an opposing checker exists ahead
+                    if (game.pips[j].owner === game.otherPlayer() && game.pips[j].size >= 1) {
+                        rank -= pipDistance(BAR[game.player], i);
                         break;
                     }
                 }
@@ -58,19 +60,22 @@ function rankBoard(game: Game): number {
             if (pip.owner === game.player && pip.size > 1) {
                 rank += 5;
             }
-        });
+        }
 
         // If we send the opponent to the bar
-        rank += game.pips[0].size * 20;
+        rank += game.pips[BAR[game.otherPlayer()]].size * 20;
     }
 
     // If we bear off
-    rank += game.off.black * 20;
+    rank += game.off[game.player] * 20;
 
     return rank;
 }
 
-// TODO: test this
+/**
+ * Returns the best sequence of moves for the current player
+ * Must be called at the beginning of a turn
+ */
 export function getBestTurn(game: Game): Move[] {
     const turns = Game.getAllPossibleValidTurns(game);
 
--- olympus-bg/src/tests/PortesBot.test.ts
+++ olympus-bg/src/tests/PortesBot.test.ts
@@ -4,7 +4,7 @@ import { getBestTurn } from "../PortesBot.js";
 import { stringToPips } from "../util.js";
 
 describe("getBestTurn", () => {
-    test("Closes a door", () => {
+    test("[black] Closes a door", () => {
         const game = new Portes({
             player: "black",
             pips: stringToPips(`
@@ -12,7 +12,6 @@ describe("getBestTurn", () => {
                 5w 0 0 0 3b 0 5b 0 0 0 0 2w
             `),
             dice: [5, 3],
-            moves: [],
         });
 
         const turn = getBestTurn(game);
@@ -20,6 +19,204 @@ describe("getBestTurn", () => {
         expect(turn).toMatchObject([
             { from: 8, to: 3 },
             { from: 6, to: 3 },
+        ]);
+    });
+
+    test("[white] Closes a door", () => {
+        const game = new Portes({
+            player: "white",
+            pips: stringToPips(`
+                5b 0 0 0 3w 0 5w 0 0 0 0 2b
+                5w 0 0 0 3b 0 5b 0 0 0 0 2w
+            `),
+            dice: [5, 3],
+        });
+
+        const turn = getBestTurn(game);
+
+        expect(turn).toMatchObject([
+            { from: 17, to: 22 },
+            { from: 19, to: 22 },
+        ]);
+    });
+
+    test("[black] Moves home in endgame", () => {
+        const game = new Portes({
+            player: "black",
+            pips: stringToPips(`
+                0 0 0 0 0 0 0 0 0 0 0 15w
+                0 0 0 1b 1b 1b 2b 2b 2b 2b 2b 2b
+            `),
+            dice: [3, 2],
+        });
+
+        const turn = getBestTurn(game);
+
+        expect(turn).toMatchObject([
+            { from: 9, to: 6 },
+            { from: 8, to: 6 },
+        ]);
+    });
+
+    test("[white] Moves home in endgame", () => {
+        const game = new Portes({
+            player: "white",
+            pips: stringToPips(`
+                0 0 0 1w 1w 1w 2w 2w 2w 2w 2w 2w
+                0 0 0 0 0 0 0 0 0 0 0 15b
+            `),
+            dice: [3, 2],
+        });
+
+        const turn = getBestTurn(game);
+
+        expect(turn).toMatchObject([
+            { from: 16, to: 19 },
+            { from: 17, to: 19 },
+        ]);
+    });
+
+    test("[black] Defends open checkers", () => {
+        const game = new Portes({
+            player: "black",
+            pips: stringToPips(`
+                5b 0 0 0 3w 0 5w 1b 0 0 0 1b
+                5w 0 0 0 3b 0 3b 1b 1b 0 0 2w
+            `),
+            dice: [4, 1],
+        });
+
+        const turn = getBestTurn(game);
+
+        expect(turn).toMatchObject([
+            { from: 24, to: 20 },
+            { from: 5, to: 4 },
+        ]);
+    });
+
+    test("[white] Defends open checkers", () => {
+        const game = new Portes({
+            player: "white",
+            pips: stringToPips(`
+                5b 0 0 0 3w 0 3w 1w 1w 0 0 2b
+                5w 0 0 0 3b 0 5b 1w 0 0 0 1w
+            `),
+            dice: [4, 1],
+        });
+
+        const turn = getBestTurn(game);
+
+        expect(turn).toMatchObject([
+            { from: 1, to: 5 },
+            { from: 20, to: 21 },
+        ]);
+    });
+
+    test("[black] Defends furthest open checkers", () => {
+        const game = new Portes({
+            player: "black",
+            pips: stringToPips(`
+                5b 0 0 0 3w 0 5w 0 0 0 0 1b
+                5w 0 0 0 3b 0 4b 1b 1b 0 0 2w
+            `),
+            dice: [4, 1],
+        });
+
+        const turn = getBestTurn(game);
+
+        expect(turn).toMatchObject([
+            { from: 8, to: 4 },
+            { from: 6, to: 5 },
+        ]);
+    });
+
+    test("[white] Defends furthest open checkers", () => {
+        const game = new Portes({
+            player: "white",
+            pips: stringToPips(`
+                5b 0 0 0 3w 0 4w 1w 1w 0 0 2b
+                5w 0 0 0 3b 0 5b 0 0 0 0 1w
+            `),
+            dice: [4, 1],
+        });
+
+        const turn = getBestTurn(game);
+
+        expect(turn).toMatchObject([
+            { from: 17, to: 21 },
+            { from: 19, to: 20 },
+        ]);
+    });
+
+    test("[black] Hits opponent", () => {
+        const game = new Portes({
+            player: "black",
+            pips: stringToPips(`
+                6b 0 0 0 1w 1b 5w 2w 0 0 0 0
+                5w 0 0 0 3b 0 5b 0 0 0 0 2w
+            `),
+            dice: [4, 1],
+        });
+
+        const turn = getBestTurn(game);
+
+        expect(turn).toMatchObject([
+            { from: 18, to: 17 },
+            { from: 17, to: 13 },
+        ]);
+    });
+
+    test("[white] Hits opponent", () => {
+        const game = new Portes({
+            player: "white",
+            pips: stringToPips(`
+                5b 0 0 0 3w 0 5w 0 0 0 0 2b
+                6w 0 0 0 1b 1w 5b 2b 0 0 0 0
+            `),
+            dice: [4, 1],
+        });
+
+        const turn = getBestTurn(game);
+
+        expect(turn).toMatchObject([
+            { from: 7, to: 8 },
+            { from: 8, to: 12 },
+        ]);
+    });
+
+    test("[black] Bears off", () => {
+        const game = new Portes({
+            player: "black",
+            pips: stringToPips(`
+                0 0 0 0 0 0 2w 2w 2w 2w 2w 2w
+                0 0 0 0 0 0 2b 2b 2b 2b 2b 2b
+            `),
+            dice: [5, 4],
+        });
+
+        const turn = getBestTurn(game);
+
+        expect(turn).toMatchObject([
+            { from: 4, to: 0 },
+            { from: 5, to: 0 },
+        ]);
+    });
+
+    test("[white] Bears off", () => {
+        const game = new Portes({
+            player: "white",
+            pips: stringToPips(`
+                0 0 0 0 0 0 2w 2w 2w 2w 2w 2w
+                0 0 0 0 0 0 2b 2b 2b 2b 2b 2b
+            `),
+            dice: [5, 4],
+        });
+
+        const turn = getBestTurn(game);
+
+        expect(turn).toMatchObject([
+            { from: 20, to: 25 },
+            { from: 21, to: 25 },
         ]);
     });
 });
